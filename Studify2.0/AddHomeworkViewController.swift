cd795b4879a647b48850774c3257f063ed1db18f
mg2005 <warrior.catcake@gmail.com>
2018-12-27 15:31:07 -0800

    added file for add HW view controller

---
+++ Studify2.0/AddHomeworkViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AddHomeworkViewController.swift
+//  Studify2.0
+//
+//  Created by Mahika Gupta on 12/27/18.
+//  Copyright © 2018 RosalieW. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddHomeworkViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        <#code#>
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        <#code#>
+    }
+    
+    
+}
